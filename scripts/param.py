fa2bf504b449256a20f95c7ccfa2b34559d036b3
RamsesKools <ramseskools@hotmail.com>
2015-09-25 17:37:17 +0200

    Changed the provided python scripts to be able to send a bundle of simulation to the supercomputer. It works for different epsilon with different delta. Still need to test it properly

---
+++ scripts/param.py
@@ -0,0 +1,57 @@
+#param.py
+#Parameters for the simulation
+
+#name of this group of simulations
+SimName = 'RK_1patch'
+
+#defines the number of cycles
+Ncycle1 = 1000
+Ncycle2 = 1000
+
+# defines which epsilons to simulate
+eps =[]
+for x in range(10,21,1):
+	eps.append(x)
+
+# defines which deltas to simulate
+delta =[]
+for x in range(10,65,5):
+	delta.append(x)
+
+# defines how many sites to simulate
+Nsites = 1
+# defines the maximum number of states in the program
+Maxinterfaces = 15
+
+filename = "lambda_test.inp"
+
+
+# defines how the lambda file should be made (works for lambda_b and lambda_u)
+def create_lambda_file (Epsilon, filename):
+	f = open(filename, 'w')
+
+	eps = Epsilon * 0.3
+	step = (Epsilon - eps) / (Maxinterfaces)
+
+	#if step < 1:
+	#	step = 1
+
+	Ninterfases = ((Epsilon - eps) / step)
+
+	f.write('%f \n' % Ninterfases)
+	f.write('%f \n' % eps)
+
+	for x in range(1,int(Ninterfases)):
+		f.write('%f \n' % eps)
+		eps += step
+
+	f.close()
+	return
+
+def create_lambda_u ():
+	f = open("lambda_u.inp", "w")
+
+	f.write('8 \n0 \n0 \n 0.000000001 \n0.000001 \n0.001 \n0.1 \n1.0 \n1.0\n 2.0\n')
+
+	f.close()
+	return
